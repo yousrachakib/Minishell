349a5a8afba326d9ac6c6fa25dc71ed12cd7cb44
mben-sal <manarbensalah7@gmail.com>
2023-07-19 10:11:15 +0100

    regle seg de -n

--- execution/builtins/echo.c
+++ execution/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: mben-sal <mben-sal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 19:17:30 by mben-sal          #+#    #+#             */
-/*   Updated: 2023/07/17 18:59:30 by mben-sal         ###   ########.fr       */
+/*   Updated: 2023/07/19 10:01:32 by mben-sal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ int ft_check_ln(char *str)
 {
 	int i = 0;
 	
-	if(str[i] == '-' && str[i + 1] == 'n')
+	if(str[i] != '\0' && str[i] == '-' && str[i + 1] == 'n' )
 	{
 		i++;
-		while (str[i] && str[i] == 'n')
+		while (str[i] != '\0'&& str[i] == 'n')
 			i++;
 		if(str[i] == '\0')
-				return(1);
+			return(1);
 		else if(str[i] != 'n')
 			return(0);
 	}
@@ -34,14 +34,16 @@ int ft_echo(char **cmd)
 	int i = 1;
 	if(!cmd[i])
 		printf("\n");
-	while(cmd[i] && ft_check_ln(cmd[i]) == 1)
+	while(cmd[i] != (void *)0 && ft_check_ln(cmd[i]) == 1)
 	{
 		i++;
 		if(cmd[i] && !ft_check_ln(cmd[i]))
+		{
 			ft_putstr_fd(cmd[i] , 1);
-		if(cmd[i][0])
 			ft_putstr_fd(" " , 1);
-		i++;
+			i++;
+		}
+		// if(cmd[i][0])
 	}
 	if(cmd[i] && !ft_check_ln(cmd[i]) && cmd)
 	{
